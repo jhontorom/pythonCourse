e857792f0391f7b7e697373374de4a4ccd54b6af
JhonToroM <jhontoro.m7@gmail.com>
2018-01-04 16:50:54 -0600

    holamundo code

---
+++ exam1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env oython3
+#-*- coding: utf -8 -*-
+#----------------------------------------------------------
+__author__ = "Jhon Toro"
+__copyright__ = "Copyright 2018, ST Projects"
+__credits__ = "j"
+__license__ = "GLP 2"
+__version__ = "1.0.1"
+__maintainer__ = "x"
+__email__ = "x@gmail.com"
+__status__ = "active"
+__execution__ = "python"
+#----------------------------------------------------------
+# Library
+import random
+
+
+#----------------------------------------------------------
+def myfirstHello():
+	print("HolaMundo")
+
+#----------------------------------------------------------
+#if __name__ == "__main__"
+myfirstHello()
+#----------------------------------------------------------
